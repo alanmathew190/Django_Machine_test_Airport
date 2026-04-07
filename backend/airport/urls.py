ec9189a58ba4e7866e867c6dc6a753ad55218f37
Alan <alanmathew190@gmail.com>
2026-04-07 19:13:36 +0530

    proper comments so far

--- backend/airport/urls.py
+++ backend/airport/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.add_route, name='add_route'),
-    path('longest/',views.longest_route,name='longest_route')
+    path('', views.add_route, name='add_route'),  #frontpage shows add_route
+    path('longest/',views.longest_route,name='longest_route')  #to get longest route 
 ]
--- backend/airport/views.py
+++ backend/airport/views.py
@@ -4,16 +4,17 @@ from .form import AirportRouteForm
 
 def add_route(request):
     if request.method == "POST":  
-        form = AirportRouteForm(request.POST)  
-        if form.is_valid():
+        form = AirportRouteForm(request.POST)    #import form skeleton
+        if form.is_valid():  #check valid
             form.save()
-            return redirect('add_route')
+            return redirect('add_route')  # redirected to again add route 
     else:
         form = AirportRouteForm()
 
-    return render(request, 'routes/add_route.html', {'form': form})
+    return render(request, 'routes/add_route.html', {'form': form})  #pass the form value add_route.html
 
 
 def longest_route(request):
     route = AirportRoute.objects.order_by('-duration').first()  #sort by asc and get the first one
     return render(request, 'routes/longest.html', {'route': route}) #pass the first to longest.html
+
