50daeacde11f8449d791bcab6d5957d621d55047
Alan <alanmathew190@gmail.com>
2026-04-07 18:27:31 +0530

    model for airport route added

--- backend/airport/models.py
+++ backend/airport/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class AirportRoute(models.Model):
+    airport_code = models.CharField(max_length=10)
+    position = models.IntegerField()
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return self.airport_code
