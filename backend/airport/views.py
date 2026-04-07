c8d8a6177f93b7454d01872a83c949ba913d7abf
Alan <alanmathew190@gmail.com>
2026-04-07 18:36:11 +0530

    add route view logic implemented

--- backend/airport/views.py
+++ backend/airport/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import AirportRoute
+from .form import AirportRouteForm
 
-# Create your views here.
+def add_route(request):
+    if request.method == "POST":
+        form = AirportRouteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_route')
+    else:
+        form = AirportRouteForm()
+
+    return render(request, 'routes/add_route.html', {'form': form})
