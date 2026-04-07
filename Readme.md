81fca286d768640ebe9ce7361c66b592cc46e72f
Alan <alanmathew190@gmail.com>
2026-04-07 19:40:31 +0530

    add Readme.md

---
+++ Readme.md
@@ -0,0 +1,14 @@
+Django Machine Test – Flight Routes System
+
+How To Run :
+    Clone the repo
+    Install Django → pip install django
+    Run → python manage.py migrate
+    Start → python manage.py runserver
+    Open → http://127.0.0.1:8000/
+
+Features
+    Add airport routes
+    Find Nth node (left/right)
+    Longest route
+    Shortest route between positions
--- backend/airport/urls.py
+++ backend/airport/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.add_route, name='add_route'),  #frontpage shows add_route
     path('longest/',views.longest_route,name='longest_route'),  #to get longest route
-    path('search/',views.search_node,name='search_node'),
+    path('search/',views.search_node,name='search_node'), # to search the nth node from left or right
 ]
